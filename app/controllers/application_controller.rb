3d443a7084920715f48caf6f07d92ec1514f4bd7
Fares Harrazi <m.fares.harrazi@gmail.com>
2025-01-07 15:30:12 +0200

    add sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
+  def after_sign_out_path_for(_resource_or_scope)
+    new_session_path(:user)
+  end
 end
