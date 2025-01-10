c85fafa415e28804a421fa4ada83c5d5cd9f635b
Fares Harrazi <m.fares.harrazi@gmail.com>
2025-01-10 18:03:05 +0200

    change api provier

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,17 +1,27 @@
 class Stock < ApplicationRecord
-    def self.new_lookup(ticker_symbol)
-        api_key = Rails.application.credentials.finazon[:api_key]
-        url = "https://api.finazon.io/latest/finazon/us_stocks_essential/price"
-        
-        response = HTTParty.get(url, query: { apikey: api_key, ticker: ticker_symbol })
-        # puts "Response Body: #{response.body}" # Debug: Print raw response
-        # puts "Parsed Response: #{response.parsed_response}" # Debug: Print parsed response
+  def self.new_lookup(ticker_symbol)
+    api_key = Rails.application.credentials.alphavantage[:api_key]
+    url = "https://www.alphavantage.co/query"
+    
+    response = HTTParty.get(url, query: {
+      function: 'TIME_SERIES_DAILY',
+      symbol: ticker_symbol,
+      apikey: api_key
+    })
+
+    # Debug: Print raw response if needed
+    # puts "Response Body: #{response.body}" 
+    # puts "Parsed Response: #{response.parsed_response}"
+
+    if response.success? && response.parsed_response['Time Series (Daily)']
+      # Extract the latest date
+      time_series = response.parsed_response['Time Series (Daily)']
+      latest_date = time_series.keys.max
+      latest_price = time_series[latest_date]['4. close']
       
-        # Check if the response contains the price
-        if response.success? && response.parsed_response['p']
-          response.parsed_response['p']
-        else
-          raise "Stock price not found for ticker: #{ticker_symbol}"
-        end
-      end
+      latest_price
+    else
+      raise "Stock price not found for ticker: #{ticker_symbol}"
+    end
+  end
 end
